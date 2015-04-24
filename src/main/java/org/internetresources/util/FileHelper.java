aed6749f6bf9cbd766c28f13cbae74fba4206ab4
boly38 <boly38@gmail.com>
2015-04-24 11:25:45 +0200

    upd junit/mockito/fest assert versions add FileHelperTest UTest

--- src/main/java/org/internetresources/util/FileHelper.java
+++ src/main/java/org/internetresources/util/FileHelper.java
@@ -1,21 +1,32 @@
 package org.internetresources.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class FileHelper {
+    private static Log LOG = LogFactory.getLog(FileHelper.class.getName());
     final String READ_MODE = "r";
+
+    RandomAccessFile _getReadRandomAccessFile(File file) throws FileNotFoundException {
+        return new RandomAccessFile(file, READ_MODE);
+    }
 
     public String readStringFromFile(File file) {
         String line = null;
         RandomAccessFile raf = null;
         try {
-            raf = new RandomAccessFile(file, READ_MODE);
+            raf = _getReadRandomAccessFile(file);
             byte[] buffer = new byte[(int) raf.length()];
             raf.read(buffer);
             line = new String(buffer);
         } catch (IOException e) {
+            LOG.error("IOException while reading "+ file.getAbsolutePath());
+            return null;
         } finally {
             if (raf != null) {
                 try {
@@ -26,4 +37,5 @@ public class FileHelper {
         }
         return line;
     }
+
 }
--- src/test/java/org/internetresources/util/FileHelperTest.java
+++ src/test/java/org/internetresources/util/FileHelperTest.java
@@ -1,24 +1,46 @@
 package org.internetresources.util;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import org.junit.Test;
 
 public class FileHelperTest {
 
     @Test
-    public void should_read_file() {
+    public void should_read_file() throws FileNotFoundException {
         // GIVEN
-        FileHelper fHelper = new FileHelper();
+        FileHelper fHelper = spy(new FileHelper());
         File f = new File("src/test/resources/fileTest.txt");
 
         // WHEN
         String readString = fHelper.readStringFromFile(f);
 
         // THEN
+        verify(fHelper)._getReadRandomAccessFile(f);
         assertThat(readString).isEqualTo("A_A01D48B80CE2\nA_A01D48B80CE2bis");
     }
 
+    @Test
+    public void should_return_null_on_read_file_exception() throws IOException {
+        // GIVEN
+        FileHelper fHelper = spy(new FileHelper());
+        File f = new File("src/test/resources/fileTest.txt");
+        RandomAccessFile raf = mock(RandomAccessFile.class);
+        doReturn(raf).when(fHelper)._getReadRandomAccessFile(f);
+        doThrow(new IOException("c'est balot")).when(raf).read(any(byte[].class));
+
+        // WHEN
+        String readString = fHelper.readStringFromFile(f);
+
+        // THEN
+        verify(fHelper)._getReadRandomAccessFile(f);
+        assertThat(readString).isNull();
+        verify(raf).close();
+    }
 }
